afd16101b0cc6fade2fef0e9fba5149fafc7970b
Juan Carlos <juandev33@gmail.com>
2023-01-14 18:31:26 -0300

    icons

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,9 @@
+import { MagnifyingGlass } from "phosphor-react";
+
 export function Search() {
     return (
         <div className="searchInput">
+            <MagnifyingGlass size={32} color="rgb(1, 147, 215)"/>
             <input type="text" placeholder="Search GitHub username..." />
             <button>Search</button>
         </div>
